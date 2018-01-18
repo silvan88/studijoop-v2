4ad52e5f2eeca907eea0228abed05c94e1ec5e8b
Silvan Snik <silvan.snik@luminis.eu>
2018-01-18 23:25:40 +0100

    added footer navigation and pages

--- src/app/nav-footer/nav-footer.component.ts
+++ src/app/nav-footer/nav-footer.component.ts
@@ -1,23 +1,57 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
-import {filter, pairwise} from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'studijoop-nav-footer',
   templateUrl: './nav-footer.component.html',
   styleUrls: ['./nav-footer.component.scss']
 })
-export class NavFooterComponent implements OnInit {
+export class NavFooterComponent {
 
-  constructor(private router: Router) {
-  }
+  public showFooter = false;
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
-    this.router.events
-      .pipe(filter(e => e.constructor.name === 'RoutesRecognized'))
-      .pipe(pairwise())
-      .subscribe((e: any[]) => {
-        localStorage.setItem('referrer', e[0].urlAfterRedirects);
-      });
+    if (this.router.url !== '') {
+      this.showFooter = true;
+    }
+  }
+
+  goToNextPage() {
+    const currentRoute = this.router.url.substr(1);
+    const nextRoute = this.router.config[this.getRouteIndex(currentRoute) + 1];
+    const goToRoute = nextRoute ? nextRoute.path : currentRoute;
+
+    this.router.navigate(['/' + goToRoute]);
+  }
+
+  goToPreviousPage() {
+    const currentRoute = this.router.url.substr(1);
+    let nextRouteIndex = this.getRouteIndex(currentRoute);
+    nextRouteIndex = nextRouteIndex === 1 ? nextRouteIndex : nextRouteIndex - 1;
+
+    const prevRoute = this.router.config[nextRouteIndex];
+    const goToRoute = prevRoute ? prevRoute.path : currentRoute;
+
+    this.router.navigate(['/' + goToRoute]);
+  }
+
+  goToFirstPage() {
+    const firstRoute = this.router.config[1];
+    this.router.navigate(['/' + firstRoute.path]);
+  }
+
+  goToLastPage() {
+    const firstRoute = this.router.config[this.router.config.length - 1];
+    this.router.navigate(['/' + firstRoute.path]);
+  }
+
+  private getRouteIndex(currentRoute: string): number {
+    for (let i = 0; i < this.router.config.length; i++) {
+      if (this.router.config[i].path === currentRoute) {
+        return i;
+      }
+    }
   }
 }
